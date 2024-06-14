b30ccba44ccd24e3d5ac238cd3d5a7c702e425de
KMNRao <kmohanarao09@gmail.com>
2024-06-14 15:11:37 +0530

    roboshop-infra

--- 01-vpc/main.tf
+++ 01-vpc/main.tf
@@ -1,19 +1,9 @@
-terraform {
-    required_providers {
-        aws = {
-            source = "hashicorp/aws"
-            version = "5.31.0"
-        }
-    }
-
-    backend "s3" {
-        bucket = "mohana-state-dev"
-        key    = "vpc"
-        region = "us-east-1"
-        dynamodb_table = "mohana-locking-dev"
-    }
-}
-
-provider "aws" {
-    region ="us-east-1"
+module "reboshop" {
+    #source = "../terraform-aws-vpc"
+    source = "git::htpps:://github.com/DevOps-Practice-Test2/terraform-aws-vpc.git?ref=main"
+    project_name = var.project_name
+    environment = var.environment
+    common_tags = var.common_tags
+    vpc_tags = var.vpc_tags
+    
 }
